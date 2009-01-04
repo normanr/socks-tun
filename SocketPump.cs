1c61b021dccd3e6aab64567c709257e4f6cfaf30
Norman Rasmussen <norman@rasmussen.co.za>
2009-01-04 14:06:10 +0000

    Initial check-in

---
+++ SocketPump.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace SocksTun
+{
+	public static class SocketPump
+	{
+		public static void Pump(Socket s1, Socket s2)
+		{
+			var wh = new ManualResetEvent(false);
+			var p1 = new SinglePump(s1, s2, wh);
+			var p2 = new SinglePump(s2, s1, wh);
+			p1.Pump();
+			p2.Pump();
+			while (s1.Connected && s2.Connected && !p1.Error && !p2.Error)
+			{
+				wh.WaitOne(10000);
+			}
+		}
+
+		class SinglePump
+		{
+			const int bufferSize = 10000;
+			private readonly Socket s1;
+			private readonly Socket s2;
+			private readonly EventWaitHandle errorEvent;
+			private readonly byte[] buf = new byte[bufferSize];
+			public bool Error { get; private set; }
+
+			public SinglePump(Socket s1, Socket s2, EventWaitHandle errorEvent)
+			{
+				this.s1 = s1;
+				this.s2 = s2;
+				this.errorEvent = errorEvent;
+			}
+
+			public void Pump()
+			{
+				try
+				{
+					if (!s1.Connected || !s2.Connected) return;
+					s1.BeginReceive(buf, 0, bufferSize, SocketFlags.None, ReceiveCallback, null);
+				}
+				catch (ObjectDisposedException)
+				{
+					Error = true;
+					errorEvent.Set();
+				}
+				catch (SocketException)
+				{
+					Error = true;
+					errorEvent.Set();
+				}
+			}
+
+			private void ReceiveCallback(IAsyncResult ar)
+			{
+				try
+				{
+					if (!s1.Connected || !s2.Connected) return;
+					var bytesReceived = s1.EndReceive(ar);
+					if (bytesReceived > 0)
+					{
+						s2.Send(buf, 0, bytesReceived, SocketFlags.None);
+						Pump();
+					}
+					else
+					{
+						Error = true;
+						errorEvent.Set();
+					}
+				}
+				catch (ObjectDisposedException)
+				{
+					Error = true;
+					errorEvent.Set();
+				}
+				catch (SocketException)
+				{
+					Error = true;
+					errorEvent.Set();
+				}
+			}
+		}
+	}
+}
---
+++ SocksTunService.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace SocksTun
+{
+	partial class SocksTunService
+	{
+		/// <summary> 
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Component Designer generated code
+
+		/// <summary> 
+		/// Required method for Designer support - do not modify 
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+			// 
+			// backgroundWorker1
+			// 
+			this.backgroundWorker1.WorkerSupportsCancellation = true;
+			this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+			this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+			// 
+			// SocksTunService
+			// 
+			this.ServiceName = "Service1";
+
+		}
+
+		#endregion
+
+		private System.ComponentModel.BackgroundWorker backgroundWorker1;
+	}
+}
