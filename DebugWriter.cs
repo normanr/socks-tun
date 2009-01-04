e855d9aee98a2e42a132aa75aa333138ca205f7c
Norman Rasmussen <norman@rasmussen.co.za>
2009-01-04 20:02:43 +0000

    Update TunTapDevice and add a port to the service to view the log

---
+++ DebugWriter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace SocksTun
+{
+	class DebugWriter
+	{
+		public TextWriter Writer { get; set; }
+		public int LogLevel { get; set; }
+		public int MaxQueuedMessages = 20;
+		public readonly StringWriter Status = new StringWriter();
+		public readonly BlockingQueue<string> Queue = new BlockingQueue<string>();
+
+		private const string dateTimeFormat = "yyyy'-'MM'-'dd' 'HH':'mm':'ss";
+		public void Log(int level, string message)
+		{
+			if (level >= LogLevel) return;
+			var line = DateTime.Now.ToString(dateTimeFormat) + ": " + message;
+			if (Writer != null)
+				Writer.WriteLine(line);
+			else if (level < 1)
+			{
+				Status.WriteLine(line);
+			}
+			else
+			{
+				Queue.Enqueue(line);
+				var count = Queue.Count;
+				if (count > 0 && count > MaxQueuedMessages)
+					Queue.Dequeue();
+			}
+		}
+
+		public void Log(int level, string format, params object[] args)
+		{
+			Log(level, string.Format(format, args));
+		}
+	}
+}
--- SocksTunService.cs
+++ SocksTunService.cs
@@ -19,88 +19,89 @@ namespace SocksTun
 		public SocksTunService()
 		{
 			InitializeComponent();
+			debug.LogLevel = 2;
 		}
 
 		public void Run(string[] args)
 		{
+			debug.Writer = Console.Out;
 			OnStart(args);
-			Console.WriteLine("Press enter to exit");
+			debug.Log(-1, "SocksTun running in foreground mode, press enter to exit");
 			Console.ReadLine();
 			OnStop();
 		}
 
-		private const int debug = 1;
 		private const int bufferSize = 10000;
+
+		private readonly DebugWriter debug = new DebugWriter();
+		private readonly byte[] buf = new byte[bufferSize];
+		private readonly Dictionary<KeyValuePair<IPAddress, int>, int> mappings = new Dictionary<KeyValuePair<IPAddress, int>, int>();
+		private readonly Queue<KeyValuePair<DateTime, KeyValuePair<IPAddress, int>>> mappingCleanUp = new Queue<KeyValuePair<DateTime, KeyValuePair<IPAddress, int>>>();
+		private readonly EventWaitHandle stopEvent = new ManualResetEvent(false);
+
+		private TunTapDevice tunTapDevice;
+		private FileStream tap;
+		private TcpListener transparentSocksServer;
+		private int transparentSocksPort;
+		private TcpListener logServer;
 
 		protected override void OnStart(string[] args)
 		{
-			server = new TcpListener(IPAddress.Any, 59000);
-			server.Server.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, 1);
-			server.Start();
-			serverPort = ((IPEndPoint)server.LocalEndpoint).Port;
-			Console.WriteLine("server.Port = " + serverPort);
-			server.BeginAcceptSocket(NewSocket, null);
+			transparentSocksServer = new TcpListener(IPAddress.Any, 59000);
+			transparentSocksServer.Server.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, 1);
+			transparentSocksServer.Start();
+			transparentSocksPort = ((IPEndPoint)transparentSocksServer.LocalEndpoint).Port;
+			debug.Log(0, "TransparentSocksPort = " + transparentSocksPort);
+			transparentSocksServer.BeginAcceptSocket(NewTransparentSocksConnection, null);
 
-			var tunTapDevice = new TunTapDevice(null);
-			Console.WriteLine("tunTapDevice.HumanName = " + tunTapDevice.HumanName);
+			if (debug.Writer == null)
+			{
+				logServer = new TcpListener(IPAddress.Loopback, 58000);
+				logServer.Server.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, 1);
+				logServer.Start();
+				debug.Log(0, "LogPort = " + ((IPEndPoint) logServer.LocalEndpoint).Port);
+				logServer.BeginAcceptTcpClient(NewLogConnection, null);
+			}
 
-			var mtu = 0;
-			var result = tunTapDevice.GetMtu(ref mtu);
-			Console.WriteLine("tunTapDevice.GetMtu = " + result + ":" + mtu);
+			tunTapDevice = new TunTapDevice(null);
+			debug.Log(0, "Name = " + tunTapDevice.Name);
+			debug.Log(0, "Guid = " + tunTapDevice.Guid.ToString("B"));
+			debug.Log(0, "Mac = " + tunTapDevice.GetMac());
+			debug.Log(0, "Version = " + tunTapDevice.GetVersion());
+			debug.Log(0, "Mtu = " + tunTapDevice.GetMtu());
 
 			var localIP = IPAddress.Parse("10.3.0.1");
 			var remoteNetwork = IPAddress.Parse("0.0.0.0");
 			var remoteNetmask = IPAddress.Parse("0.0.0.0");
-			result = tunTapDevice.ConfigTun(localIP, remoteNetwork, remoteNetmask);
-			Console.WriteLine("tunTapDevice.ConfigTun = " + result);
-			if (!result) return;
+			tunTapDevice.ConfigTun(localIP, remoteNetwork, remoteNetmask);
 
 			var adapterNetmask = IPAddress.Parse("255.255.255.0");
 			var dhcpServerAddr = IPAddress.Parse("10.3.0.255");
 			var dhcpLeaseTime = 86400 * 365; // one year
-			result = tunTapDevice.ConfigDhcpMasq(localIP, adapterNetmask, dhcpServerAddr, dhcpLeaseTime);
-			Console.WriteLine("tunTapDevice.ConfigDhcpMasq = " + result);
-			if (!result) return;
+			tunTapDevice.ConfigDhcpMasq(localIP, adapterNetmask, dhcpServerAddr, dhcpLeaseTime);
 
-			result = tunTapDevice.SetMediaStatus(true);
-			Console.WriteLine("tunTapDevice.SetMediaStatus = " + result);
-			if (!result) return;
+			tunTapDevice.SetMediaStatus(true);
 
-			tap = new FileStream(tunTapDevice.Handle, FileAccess.ReadWrite, bufferSize, true);
-
+			tap = tunTapDevice.Stream;
+			mappingCleanupTimer.Enabled = true;
 			backgroundWorker1.RunWorkerAsync();
 		}
 
 		protected override void OnStop()
 		{
+			mappingCleanupTimer.Enabled = false;
 			if (backgroundWorker1 == null || !backgroundWorker1.IsBusy) return;
 			backgroundWorker1.CancelAsync();
 			tap.Close();
 			stopEvent.WaitOne();
 		}
 
-		private TcpListener server;
-		private int serverPort;
-		private FileStream tap;
-
-		private readonly byte[] buf = new byte[bufferSize];
-		private readonly Dictionary<KeyValuePair<IPAddress, int>, int> mappings = new Dictionary<KeyValuePair<IPAddress, int>, int>();
-		private readonly Queue<KeyValuePair<DateTime, KeyValuePair<IPAddress, int>>> mappingCleanUp = new Queue<KeyValuePair<DateTime, KeyValuePair<IPAddress, int>>>();
-		private readonly EventWaitHandle stopEvent = new ManualResetEvent(false);
-
 		private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
 		{
 			while (!backgroundWorker1.CancellationPending)
 			{
-				while (mappingCleanUp.Count > 0 && mappingCleanUp.Peek().Key < DateTime.Now)
-				{
-					var key = mappingCleanUp.Dequeue().Value;
-					if (mappings.ContainsKey(key))
-						mappings.Remove(key);
-				}
-
 				var bytesRead = tap.Read(buf, 0, bufferSize);
-				if (debug > 0) WriteBuffer(">", buf, bytesRead);
+				WriteBuffer(">", buf, bytesRead);
 
 				var packetOffset = 0;
 
@@ -128,12 +129,12 @@ namespace SocksTun
 							var sourcePort = IPAddress.NetworkToHostOrder(BitConverter.ToInt16(buf, sourcePortOffset)) & 0xffff;
 							var destinationPort = IPAddress.NetworkToHostOrder(BitConverter.ToInt16(buf, destinationPortOffset)) & 0xffff;
 
-							if (sourcePort != serverPort)
+							if (sourcePort != transparentSocksPort)
 							{
 								var key = new KeyValuePair<IPAddress, int>(destination, sourcePort);
 								if (!mappings.ContainsKey(key))
 									mappings[key] = destinationPort;
-								destinationPort = serverPort;
+								destinationPort = transparentSocksPort;
 							}
 							else
 							{
@@ -167,9 +168,14 @@ namespace SocksTun
 				SetArray(buf, checkSumOffset, BitConverter.GetBytes((ushort)0));
 				SetArray(buf, checkSumOffset, BitConverter.GetBytes(generateIPChecksum(buf, headerLength)));
 
-				if (debug > 0) WriteBuffer("<", buf, bytesRead);
+				WriteBuffer("<", buf, bytesRead);
 				tap.Write(buf, 0, bytesRead);
 			}
+		}
+
+		private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+		{
+			stopEvent.Set();
 		}
 
 		private static ushort generateIPChecksum(byte[] arBytes, int nSize)
@@ -199,58 +205,12 @@ namespace SocksTun
 			return (ushort)(~chksum);
 		}
 
-		private void NewSocket(IAsyncResult ar)
-		{
-			var client = server.EndAcceptSocket(ar);
-			server.BeginAcceptSocket(NewSocket, null);
-
-			var localEndPoint = (IPEndPoint)client.LocalEndPoint;
-			var remoteEndPoint = (IPEndPoint)client.RemoteEndPoint;
-			var key = new KeyValuePair<IPAddress, int>(remoteEndPoint.Address, remoteEndPoint.Port);
-
-			if (mappings.ContainsKey(key))
-			{
-				var remotePort = mappings[key];
-				var requestedEndPoint = new IPEndPoint(remoteEndPoint.Address, remotePort);
-				Console.WriteLine("[{0}:{1}] Request connection to {2}", localEndPoint.Address, remoteEndPoint.Port, requestedEndPoint);
-
-				try
-				{
-					var proxy = new ProxySocket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp)
-					{
-						ProxyEndPoint = new IPEndPoint(IPAddress.Loopback, 1080),
-						ProxyType = ProxyTypes.Socks4
-					};
-					proxy.Connect(requestedEndPoint);
-
-					SocketPump.Pump(client, proxy);
-
-					proxy.Close();
-					Console.WriteLine("[{0}:{1}] Closing connection to {2}", localEndPoint.Address, remoteEndPoint.Port, requestedEndPoint);
-				}
-				catch (Exception ex)
-				{
-					client.Send(Encoding.ASCII.GetBytes(ex.ToString()));
-					Console.WriteLine("[{0}:{1}] Failed connection to {2}: {3}", localEndPoint.Address, remoteEndPoint.Port, requestedEndPoint, ex);
-				}
-
-				mappingCleanUp.Enqueue(new KeyValuePair<DateTime, KeyValuePair<IPAddress, int>>(DateTime.Now.AddSeconds(30), key));
-			}
-			else
-			{
-				Console.WriteLine("[{0}] No mapping", remoteEndPoint);
-				client.Send(Encoding.ASCII.GetBytes("No mapping\r\n"));
-			}
-
-			client.Close();
-		}
-
 		public void SetArray<T>(T[] array, int offset, T[] data)
 		{
 			Array.Copy(data, 0, array, offset, data.Length);
 		}
 
-		private static void WriteBuffer(string prefix, byte[] buf, int bytesRead)
+		private void WriteBuffer(string prefix, byte[] buf, int bytesRead)
 		{
 			var packetOffset = 0;
 			var version = buf[packetOffset] >> 4;
@@ -270,31 +230,145 @@ namespace SocksTun
 					case ProtocolType.Udp:
 						var sourcePort = IPAddress.NetworkToHostOrder(BitConverter.ToInt16(buf, headerLength + 0)) & 0xffff;
 						var destinationPort = IPAddress.NetworkToHostOrder(BitConverter.ToInt16(buf, headerLength + 2)) & 0xffff;
-						Console.WriteLine("{0}: {1}: {2}:{4} -> {3}:{5}", DateTime.Now, protocol, source, destination, sourcePort, destinationPort);
+						debug.Log(2, "{0}: {1}:{3} -> {2}:{4}", protocol, source, destination, sourcePort, destinationPort);
 						break;
 					default:
-						Console.WriteLine("{0}: {1}: {2} -> {3}", DateTime.Now, protocol, source, destination);
+						debug.Log(2, "{0}: {1} -> {2}", protocol, source, destination);
 						break;
 				}
 			}
 
-			if (debug < 2) return;
+			var sb = new StringBuilder();
 			for (var i = 0; i < bytesRead; i += 0x10)
 			{
-				Console.Write(prefix + " " + i.ToString("x8") + " ");
+				sb.Append(prefix + " " + i.ToString("x8") + "  ");
 				for (var j = i; j < bytesRead && j < i + 0x10; j++)
 				{
-					if ((j & 0xf) == 0x8) Console.Write(" ");
-					Console.Write(buf[j].ToString("x2") + " ");
+					if ((j & 0xf) == 0x8) sb.Append(" ");
+					sb.Append(buf[j].ToString("x2") + " ");
 				}
-				Console.WriteLine();
+				for (var j = i + 0xf; j >= bytesRead; j--)
+				{
+					if ((j & 0xf) == 0x8) sb.Append(" ");
+					sb.Append("   ");
+				}
+				sb.Append(" ");
+				for (var j = i; j < bytesRead && j < i + 0x10; j++)
+				{
+					if ((j & 0xf) == 0x8) sb.Append(" ");
+					var b = (char)buf[j];
+					if (b < 0x20) b = '.';
+					sb.Append(b);
+				}
+				sb.AppendLine();
 			}
-			Console.WriteLine();
+			debug.Log(3, sb.ToString());
 		}
 
-		private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+		private void NewTransparentSocksConnection(IAsyncResult ar)
 		{
-			stopEvent.Set();
+			var client = transparentSocksServer.EndAcceptSocket(ar);
+			transparentSocksServer.BeginAcceptSocket(NewTransparentSocksConnection, null);
+
+			var localEndPoint = (IPEndPoint)client.LocalEndPoint;
+			var remoteEndPoint = (IPEndPoint)client.RemoteEndPoint;
+			var key = new KeyValuePair<IPAddress, int>(remoteEndPoint.Address, remoteEndPoint.Port);
+
+			if (mappings.ContainsKey(key))
+			{
+				var remotePort = mappings[key];
+				var requestedEndPoint = new IPEndPoint(remoteEndPoint.Address, remotePort);
+				debug.Log(1, "{0}:{1} requested connection to {2}", localEndPoint.Address, remoteEndPoint.Port, requestedEndPoint);
+
+				try
+				{
+					var proxy = new ProxySocket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp)
+					{
+						ProxyEndPoint = new IPEndPoint(IPAddress.Loopback, 1080),
+						ProxyType = ProxyTypes.Socks4
+					};
+					proxy.Connect(requestedEndPoint);
+
+					SocketPump.Pump(client, proxy);
+
+					proxy.Close();
+					debug.Log(1, "{0}:{1} closing connection to {2}", localEndPoint.Address, remoteEndPoint.Port, requestedEndPoint);
+				}
+				catch (Exception ex)
+				{
+					client.Send(Encoding.ASCII.GetBytes(ex.ToString()));
+					debug.Log(1, "{0}:{1} failed connection to {2}: {3}", localEndPoint.Address, remoteEndPoint.Port, requestedEndPoint, ex);
+				}
+
+				lock (mappingCleanUp)
+					mappingCleanUp.Enqueue(new KeyValuePair<DateTime, KeyValuePair<IPAddress, int>>(DateTime.Now.AddSeconds(30), key));
+			}
+			else
+			{
+				debug.Log(1, "{1} No mapping", remoteEndPoint);
+				client.Send(Encoding.ASCII.GetBytes("No mapping\r\n"));
+			}
+
+			client.Close();
+		}
+
+		private void NewLogConnection(IAsyncResult ar)
+		{
+			var client = logServer.EndAcceptTcpClient(ar);
+
+			var stream = client.GetStream();
+			var writer = new StreamWriter(stream);
+
+			var connected = true;
+
+			var buffer = new byte[0x1000];
+			AsyncCallback reader = null;
+			reader = delegate(IAsyncResult rar) {
+				try
+				{
+					var count = stream.EndRead(rar);
+					if (count > 0)
+						stream.BeginRead(buffer, 0, 0x1000, reader, null);
+					else
+						connected = false;
+				}
+				catch (SystemException)
+				{
+					connected = false;
+				}
+			};
+			stream.BeginRead(buffer, 0, 0x1000, reader, null);
+
+			try
+			{
+				writer.Write(debug.Status);
+				debug.Log(1, tunTapDevice.GetInfo());
+				writer.Flush();
+				while (connected && client.Connected)
+				{
+					string line;
+					if (!debug.Queue.TryDequeue(1000, out line)) continue;
+					writer.WriteLine(line);
+					writer.Flush();
+				}
+			}
+			catch (SystemException)
+			{
+			}
+
+			writer.Close();
+			logServer.BeginAcceptTcpClient(NewLogConnection, null);
+		}
+
+		private void mappingCleanupTimer_Tick(object sender, EventArgs e)
+		{
+			lock (mappingCleanUp)
+				while (mappingCleanUp.Count > 0 && mappingCleanUp.Peek().Key < DateTime.Now)
+				{
+					var key = mappingCleanUp.Dequeue().Value;
+					if (mappings.ContainsKey(key))
+						mappings.Remove(key);
+				}
 		}
 	}
 }
