9d0328e853ab7f23735eab431e915a52a00e27c1
Norman Rasmussen <norman@rasmussen.co.za>
2009-01-05 09:59:40 +0000

    Extract natter, connection tracker and connection handlers into their own classes

--- DebugWriter.cs
+++ DebugWriter.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 
 namespace SocksTun
@@ -19,9 +21,8 @@ namespace SocksTun
 		{
 			if (level >= LogLevel) return;
 			var line = DateTime.Now.ToString(dateTimeFormat) + ": " + message;
-			if (Writer != null)
-				Writer.WriteLine(line);
-			else if (level < 1)
+			if (Writer != null) Writer.WriteLine(line);
+			if (level < 1)
 			{
 				Status.WriteLine(line);
 			}
@@ -37,6 +38,61 @@ namespace SocksTun
 		public void Log(int level, string format, params object[] args)
 		{
 			Log(level, string.Format(format, args));
+		}
+
+		public void LogBuffer(string prefix, byte[] buf, int bytesRead)
+		{
+			var packetOffset = 0;
+			var version = buf[packetOffset] >> 4;
+			if (version == 0x4)
+			{
+				var sourceOffset = packetOffset + 12;
+				var destinationOffset = packetOffset + 16;
+
+				var headerLength = (buf[packetOffset] & 0xf) * 4;
+				var protocol = (ProtocolType)buf[packetOffset + 9];
+				var source = new IPAddress(BitConverter.ToInt32(buf, sourceOffset) & 0xffffffff);
+				var destination = new IPAddress(BitConverter.ToInt32(buf, destinationOffset) & 0xffffffff);
+
+				switch (protocol)
+				{
+					case ProtocolType.Tcp:
+					case ProtocolType.Udp:
+						var sourcePort = IPAddress.NetworkToHostOrder(BitConverter.ToInt16(buf, headerLength + 0)) & 0xffff;
+						var destinationPort = IPAddress.NetworkToHostOrder(BitConverter.ToInt16(buf, headerLength + 2)) & 0xffff;
+						Log(2, "{0}: {1}:{3} -> {2}:{4}", protocol, source, destination, sourcePort, destinationPort);
+						break;
+					default:
+						Log(2, "{0}: {1} -> {2}", protocol, source, destination);
+						break;
+				}
+			}
+
+			var sb = new StringBuilder();
+			for (var i = 0; i < bytesRead; i += 0x10)
+			{
+				sb.Append(prefix + " " + i.ToString("x8") + "  ");
+				for (var j = i; j < bytesRead && j < i + 0x10; j++)
+				{
+					if ((j & 0xf) == 0x8) sb.Append(" ");
+					sb.Append(buf[j].ToString("x2") + " ");
+				}
+				for (var j = i + 0xf; j >= bytesRead; j--)
+				{
+					if ((j & 0xf) == 0x8) sb.Append(" ");
+					sb.Append("   ");
+				}
+				sb.Append(" ");
+				for (var j = i; j < bytesRead && j < i + 0x10; j++)
+				{
+					if ((j & 0xf) == 0x8) sb.Append(" ");
+					var b = (char)buf[j];
+					if (b < 0x20) b = '.';
+					sb.Append(b);
+				}
+				sb.AppendLine();
+			}
+			Log(3, sb.ToString());
 		}
 	}
 }
---
+++ LogConnection.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SocksTun
+{
+	class LogConnection
+	{
+		private readonly TcpClient client;
+		private readonly DebugWriter debug;
+		private readonly TunTapDevice tunTapDevice;
+		private readonly NetworkStream stream;
+		private readonly byte[] buffer = new byte[0x1000];
+
+		private bool connected;
+
+		public LogConnection(TcpClient client, DebugWriter debug, TunTapDevice tunTapDevice)
+		{
+			this.client = client;
+			this.debug = debug;
+			this.tunTapDevice = tunTapDevice;
+			stream = client.GetStream();
+		}
+
+		private void ReadComplete(IAsyncResult ar)
+		{
+			try
+			{
+				var count = stream.EndRead(ar);
+				if (count > 0)
+					stream.BeginRead(buffer, 0, 0x1000, ReadComplete, null);
+				else
+					connected = false;
+			}
+			catch (SystemException)
+			{
+				connected = false;
+			}
+		}
+
+		public void Process()
+		{
+			connected = true;
+			var writer = new StreamWriter(stream);
+
+			stream.BeginRead(buffer, 0, 0x1000, ReadComplete, null);
+
+			debug.Log(1, "{0} connected to log", client.Client.RemoteEndPoint);
+			debug.Log(1, tunTapDevice.GetInfo());
+
+			try
+			{
+				writer.Write(debug.Status);
+				writer.Flush();
+				while (connected && client.Connected)
+				{
+					string line;
+					if (!debug.Queue.TryDequeue(1000, out line)) continue;
+					writer.WriteLine(line);
+					writer.Flush();
+				}
+			}
+			catch (SystemException)
+			{
+			}
+
+			writer.Close();
+		}
+	}
+}
